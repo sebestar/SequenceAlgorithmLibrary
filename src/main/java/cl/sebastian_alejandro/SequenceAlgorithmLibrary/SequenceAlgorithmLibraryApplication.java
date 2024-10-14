72c3a4b00c8f1d26492a932269b87570af370fe8
sebestar <sebastian.alejandro.itdd@gmail.com>
2024-10-13 23:29:34 -0300

    First commit

---
+++ src/main/java/cl/sebastian_alejandro/SequenceAlgorithmLibrary/SequenceAlgorithmLibraryApplication.java
@@ -0,0 +1,13 @@
+package cl.sebastian_alejandro.SequenceAlgorithmLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequenceAlgorithmLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SequenceAlgorithmLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sebastian_alejandro/SequenceAlgorithmLibrary/SequenceAlgorithmLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sebastian_alejandro.SequenceAlgorithmLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequenceAlgorithmLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
